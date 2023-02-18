28c33d182461edbae476acf4b1fa908eb46d1c87
Rizqy-617 <rizqyandriansyah2205@gmail.com>
2023-02-18 08:32:58 +0700

    Finish Repo for user

--- server/repositories/user.go
+++ server/repositories/user.go
@@ -4,12 +4,16 @@ import (
 	"waysgallery/models"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type UserRepository interface {
 	FindUsers() ([]models.User, error)
-	// ChangePassword(user models.User) (models.User, error)
-	// ChangeImage(user models.User) (models.User, error)
+	FindArtsByUserId(ID int) ([]models.Art, error)
+	FindPostByUserId(ID int) ([]models.PostResponse, error)
+	GetUser(ID int) (models.User, error)
+	GetUserDetailById(ID int) (models.UserResponse, error)
+	UpdateProfile(user models.User) (models.User, error)
 }
 
 func RepositoryUser(db *gorm.DB) *repository {
@@ -22,4 +26,37 @@ func (r *repository) FindUsers() ([]models.User, error) {
 
 	return users, err
 }
+
+func (r *repository) FindArtsByUserId(ID int) ([]models.Art, error) {
+	var art []models.Art
+	err := r.db.Where("created_by=?", ID).Find(&art).Error
+
+	return art, err
+}
+
+func (r *repository) FindPostByUserId(ID int) ([]models.PostResponse, error) {
+	var post []models.PostResponse
+	err := r.db.Where("created_by=?", ID).Preload(clause.Associations).Find(&post).Error
+
+	return post, err
+}
+
+func (r *repository) GetUser(ID int) (models.User, error) {
+	var user models.User
+	err := r.db.First(&user, ID).Error
+
+	return user, err
+}
+
+func (r *repository) GetUserDetailById(ID int) (models.UserResponse, error) {
+	var user models.UserResponse
+	err := r.db.First(&user, ID).Error
+	return user, err
+}
+
+func (r *repository) UpdateProfile(user models.User) (models.User, error) {
+	err := r.db.Model(&user).Updates(user).Error
+	return user, err
+}
+
 
